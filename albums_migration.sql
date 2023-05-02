17397995ca345520bac329ee6647ac1816ea9503
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-05-02 07:27:59 -0500

    Adding table exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,13 @@
 USE codeup_test_db;
-CREATE TABLE IF NOT EXISTS  albums (
-                        id INT UNSIGNED NOT NULL AUTO_INCREMENT,
-                        artist VARCHAR(50) NOT NULL,
-                        name VARCHAR(50) NOT NULL,
-                        content TEXT NOT NULL,
-                        release_date INT NOT NULL ,
-                        sales FLOAT NOT NULL ,
-                        genre  VARCHAR(100) NOT NULL,
-                        PRIMARY KEY (id)
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE `albums` (
+                          `id` int unsigned NOT NULL AUTO_INCREMENT,
+                          `artist` varchar(50) NOT NULL,
+                          `name` varchar(100) NOT NULL,
+                          `release_date` int NOT NULL,
+                          `sales` decimal(15,2) NOT NULL,
+                          `genre` varchar(50) NOT NULL,
+                          PRIMARY KEY (`id`)
 );
+# INSERT INTO `albums` VALUES ('Michael Jackson','Thriller',1982,51.20,'Pop');
